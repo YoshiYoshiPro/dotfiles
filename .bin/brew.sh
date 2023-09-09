c72bba8aea2c3e59b6703aeed108d56dba61a3a5
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-10 03:48:24 +0900

    change: 💥 exitコード調整

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -1,33 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 if [ "$(uname)" != "Darwin" ] ; then
-	echo "Not macOS!"
-	exit 1
+    echo "Not macOS!"
+    exit 1
 fi
 
 # App Storeにログイン
 function open_app_store {
-  echo "Please login with your Apple ID"
-  sleep 1; echo "Open the App Store."
-  sleep 1; open -a App\ Store
+    echo "Please login with your Apple ID"
+    sleep 1; echo "Open the App Store."
+    sleep 1; open -a App\ Store
 }
 
 function login_check {
-  while true; do
-    echo -n "$* [Y/n]: (default: n) "
-    read ANS
-    case $ANS in
-      [Yy]*)
-        return 0
-        ;;
-      *)
-        open_app_store
-        ;;
-    esac
-  done
+    while true; do
+        echo -n "$* [Y/n]: (default: n) "
+        read ANS
+        case $ANS in
+            [Yy]*)
+                return 0
+                ;;
+            *)
+                open_app_store
+                ;;
+        esac
+    done
 }
 
 open_app_store
 if login_check "Did you login?"; then
-  brew bundle --global
+    if brew bundle --global; then
+        echo "Successfully executed brew bundle!"
+    else
+        echo "Error: Failed to execute brew bundle."
+        exit 1
+    fi
 fi
